9fed4629dfe1c46f0e446ce67aa262d0f5a160de
R1DF <33981824+R1DF@users.noreply.github.com>
2022-12-18 21:35:11 +0400

    added configuration loader

---
+++ src/config_loader.py
@@ -0,0 +1,15 @@
+# Imports
+import toml
+
+# Configurations class
+class Configurations:
+    def __init__(self, path):
+        # Getting path and full configurations dictionary
+        self.path = path
+        self.data = toml.load(path)
+
+        # Getting inner values for ease
+        self.program_data = self.data["program"]
+        self.defaults = self.data["defaults"]
+        self.game_data = self.data["game"]
+
