80e7bcad1cbdcf32b2ccbccf042fe86910a3268a
R1DF <33981824+R1DF@users.noreply.github.com>
2022-12-18 21:30:57 +0400

    nothing much for now

---
+++ src/main.py
@@ -0,0 +1,11 @@
+"""
+The Differentiation Quiz (17.12.22)
+Coded by R1DF.
+View licence at LICENCE.md.
+
+Please run start.py if you want to play via source.
+"""
+
+class TheDifferentiationQuiz:
+    pass
+
