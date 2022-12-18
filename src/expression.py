ae8be32ba63baed6eb034976a5fc952b6fa03c2d
R1DF <33981824+R1DF@users.noreply.github.com>
2022-12-18 21:31:51 +0400

    added Expression class but it's empty right now

---
+++ src/expression.py
@@ -0,0 +1,17 @@
+# Imports
+from term import Term
+
+# Expression base
+class Expression:
+    def __init__(self):
+        self.expressions = []
+
+    def add_term(self):
+        pass
+
+    def visualize(self):
+        pass
+
+    def differentiated(self):
+        pass
+
