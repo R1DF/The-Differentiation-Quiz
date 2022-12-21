c92cefe0e6a19d537f04df710497716963e115cc
R1DF <33981824+R1DF@users.noreply.github.com>
2022-12-21 17:39:51 +0600

    Actual app class here.

--- src/main.py
+++ src/main.py
@@ -6,6 +6,17 @@ View licence at LICENCE.md.
 Please run start.py if you want to play via source.
 """
 
-class TheDifferentiationQuiz:
-    pass
+# Imports
+from interfaces.main_menu import MainMenuInterfaceCL
+from utils.system import title, clear
+from config_loader import Configurations
+from lang_loader import LanguagePack
 
+# App
+class TheDifferentiationQuiz:
+    VERSION = "1.0.0"
+    def __init__(self, configurations: Configurations, language_pack: LanguagePack):
+        self.configurations = configurations
+        self.language_pack = language_pack
+        title(f"The Differentiation Quiz [{TheDifferentiationQuiz.VERSION}]")
+        self.main_menu = MainMenuInterfaceCL(self, "mainMenu")
