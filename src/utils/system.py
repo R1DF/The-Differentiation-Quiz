73cbab044b26a5434e9f6b178128f27aaf9f44d2
R1DF <33981824+R1DF@users.noreply.github.com>
2022-12-21 17:36:54 +0600

    Added system specific functions (including PLATFORM variable)

---
+++ src/utils/system.py
@@ -0,0 +1,29 @@
+# Imports
+import os
+import platform
+import readchar
+
+# Platform
+PLATFORM = platform.system()
+
+# System functions
+def title(terminal_title=""):
+    # Only works for windows
+    if PLATFORM == "Windows":
+        os.system(f"title {terminal_title}")
+
+
+def wait(language_pack, to_exit=False):
+    print(language_pack.section_get("multipleUse")["pressToExit" if to_exit else "pressToContinue"])
+
+
+def wait_for_enter(language_pack, to_exit=False):
+    print(language_pack.section_get("multipleUse")["enterToExit" if to_exit else "enterToExit"])
+    while True:
+        if readchar.readkey() == readchar.key.ENTER:
+            break
+
+
+def clear():
+    os.system("cls" if PLATFORM == "Windows" else "clear")
+
