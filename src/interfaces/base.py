609f62beb83692ac1299825be6f1484007aab2df
R1DF <33981824+R1DF@users.noreply.github.com>
2022-12-24 01:34:53 +0600

    Got rid of default infinite looping and must_clear argument

--- src/interfaces/base.py
+++ src/interfaces/base.py
@@ -7,19 +7,14 @@ from utils.system import clear
 
 # Base
 class InterfaceCL:
-    def __init__(self, master, language_section_name, must_clear=True):
+    def __init__(self, master, language_section_name):
         self.master = master  # the App object that is linked to the interface
         self.configurations = master.configurations
         self.language_pack = master.language_pack
         self.language_section = self.language_pack.menu_get(language_section_name)
         self.continued = True  # Set to false and get to end of loop
 
-        while self.continued:
-            # This is a loop to make sure the options and visuals are still appearing in cycles
-            if must_clear:
-                clear()
-            self.load_interface()
+        self.load_interface()
 
     def load_interface(self):
         pass  # Overridden
-
