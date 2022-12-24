f5bfeb14c0537fe28337baea39ba15a1fcd2ec31
R1DF <33981824+R1DF@users.noreply.github.com>
2022-12-25 00:09:51 +0600

    Work is far from over

---
+++ src/game.py
@@ -0,0 +1,14 @@
+# Imports
+from question import Question
+
+# Game class
+class Game:
+    def __init__(
+            self,
+            amount_of_questions,
+            allowed_question_types: list[int],
+            use_negative_powers,
+            use_decimal_power
+    ):
+        pass
+
