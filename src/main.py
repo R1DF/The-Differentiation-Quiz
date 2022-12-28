eb5776ffda4c958764e6d674fa2fab7378c39867
R1DF <33981824+R1DF@users.noreply.github.com>
2022-12-28 22:12:24 +0600

    Implemented settings and language changing interfaces

--- src/main.py
+++ src/main.py
@@ -11,7 +11,9 @@ from interfaces.main_menu import MainMenuInterfaceCL
 from interfaces.game_setup import GameSetupInterfaceCL
 from interfaces.game_loader import GameLoadingInterfaceCL
 from interfaces.game_interface import GameInterfaceCL
-from interfaces.updates_check_interface import UpdatesCheckInterface
+from interfaces.updates_check_interface import UpdatesCheckInterfaceCL
+from interfaces.settings_interface import SettingsInterfaceCL
+from interfaces.language_interface import LanguageInterfaceCL
 from utils.system import title
 from config_loader import Configurations
 from lang_loader import LanguagePack
@@ -43,6 +45,14 @@ class TheDifferentiationQuiz:
         del self.current_menu
 
     def make_updates_check_interface(self):
-        self.current_menu = UpdatesCheckInterface(self, "checkingUpdates", TheDifferentiationQuiz.VERSION)
+        self.current_menu = UpdatesCheckInterfaceCL(self, "checkingUpdates", TheDifferentiationQuiz.VERSION)
         del self.current_menu
+
+    def make_settings_interface(self):
+        self.current_menu = SettingsInterfaceCL(self, "settings")
+        del self.current_menu
+
+    def make_language_interface(self, dictionary_to_modify):
+        self.current_menu = LanguageInterfaceCL(self, "language", dictionary_to_modify, TheDifferentiationQuiz.VERSION, self.language_pack.name)
+        #del self.current_menu
 
