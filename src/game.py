4fd933072b41412fa6ab98681ffb02fac6d761a8
R1DF <33981824+R1DF@users.noreply.github.com>
2022-12-28 15:15:54 +0600

    Fixed logic error

--- src/game.py
+++ src/game.py
@@ -18,6 +18,7 @@ class Game:
         self.questions = []
         self.correctly_answered = 0
         self.questions_skipped = 0
+        self.incorrectly_answered = 0
         self.skips_left = allowed_skips
 
     def generate(self, maximum_amount_of_terms, track_generation=False, question_number_text=None, success_text = ""):
@@ -26,7 +27,7 @@ class Game:
             "3": random.randint(1, 4) if 3 in self.allowed_question_types else 0
         }
         question_types["1"] = self.amount_of_questions - question_types["2"] - question_types["3"]
-        currently_available_types = [int(x) for x in question_types.keys() if x != 0]
+        currently_available_types = [int(x) for x in question_types.keys() if question_types[x] != 0]
 
         for question in range(self.amount_of_questions):
             question_type = random.choice(currently_available_types)
